d956434c2d971c54ed5fe975a334413eefaf2df0
Matt Wiebke <matt@thegainslab.com>
2025-08-15 11:57:08 -0400

    Add coach apply endpoint

--- app/api/coach/accept-coach/[relationshipId]/route.ts
+++ app/api/coach/accept-coach/[relationshipId]/route.ts
@@ -1,4 +1,4 @@
-w// /app/api/athlete/accept-coach/[relationshipId]/route.ts
+// /app/api/athlete/accept-coach/[relationshipId]/route.ts
 import { NextRequest, NextResponse } from 'next/server'
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
