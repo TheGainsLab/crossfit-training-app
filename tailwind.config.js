07355bccf4701c8fbba1c0dff5d145e822859e8c
Matt Wiebke <matt@thegainslab.com>
2025-08-27 13:14:45 -0400

    blue bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: ['bg-coral', 'text-coral'],
   theme: {
     extend: {
       colors: {
